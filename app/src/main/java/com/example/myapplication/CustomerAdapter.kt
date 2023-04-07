5e9b3840f1e1f0cb9011ef519fa22a9d8c52bb84
DESKTOP-SITCRD2\OmegaR <anton.knyazev@omega-r.com>
2023-04-07 12:02:18 +0300

    anton ydalil i vse slomalos

--- app/src/main/java/com/example/myapplication/CustomerAdapter.kt
+++ app/src/main/java/com/example/myapplication/CustomerAdapter.kt
@@ -6,11 +6,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomerAdapter (private val listrepo: List<Repo>, private val listItemClick: OnItemClickListener) : RecyclerView.Adapter<CustomerAdapter.ViewHolder>(){
+
+open class CustomerAdapter (private val listrepo: List<Repo>, private val listItemClick: OnItemClickListener) : RecyclerView.Adapter<CustomerAdapter.ViewHolder>(){
 
     inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         val nameRepoTextView = itemView.findViewById<TextView>(R.id.textlist)!!
+
         init {
             itemView.setOnClickListener(this@ViewHolder)
         }
@@ -20,10 +22,8 @@ class CustomerAdapter (private val listrepo: List<Repo>, private val listItemCli
         }
     }
 
-    interface OnItemClickListener {
-        companion object{
-        }
-        fun onItemClick(view: View, position: Int): RecyclerView.OnItemTouchListener
+    fun interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -36,47 +36,9 @@ class CustomerAdapter (private val listrepo: List<Repo>, private val listItemCli
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val itemViewModel = listrepo[position]
         holder.nameRepoTextView.text = itemViewModel.name
+//        holder.itemView.setOnClickListener {listItemClick}
     }
     override fun getItemCount(): Int {
         return listrepo.size
     }
 }
-
-//class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-//{
-//
-//    fun bind(repo: Repo, clickListener: OnItemClickListener)
-//    {
-//
-//        itemView.setOnClickListener {
-//            clickListener.onItemClicked(repo)
-//        }
-//    }
-//
-//}
-//
-//
-//class CustomerAdapter(var repos: List<Repo>, val itemClickListener: OnItemClickListener):RecyclerView.Adapter<MyHolder>() {
-//    override fun onCreateViewHolder(parent: ViewGroup, position: Int): MyHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_main,parent,false)
-//        return MyHolder(view)
-//
-//
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return repos.size
-//    }
-//
-//    override fun onBindViewHolder(myHolder: MyHolder, position: Int) {
-//        val repo = repos[position]
-//        myHolder.bind(repo,itemClickListener)
-//
-//
-//    }
-//}
-//
-//
-//interface OnItemClickListener{
-//    fun onItemClicked(repo: Repo)
-//}
--- app/src/main/java/com/example/myapplication/main/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.main
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,117 +21,41 @@ class MainActivity : BaseActivity(R.layout.activity_main), MainView,
     CustomerAdapter.OnItemClickListener {
     override val presenter: MainPresenter by providePresenter()
 
-    companion object{
+    companion object {
         fun createLauncher() = createActivityLauncher()
     }
-
-    val btnSearch = findViewById<Button>(R.id.btn_search)
-    val editTextTextPersonName = findViewById<EditText>(R.id.editTextPersonName)!!
-    val repos = service.listRepos(editTextTextPersonName.text.toString())
-
+    var listRepo: List<Repo> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editTextTextPersonName = findViewById<EditText>(R.id.editTextPersonName)!!
+        val repos = service.listRepos(editTextTextPersonName.text.toString())
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView!!.layoutManager = LinearLayoutManager(this)
 
-
+        val btnSearch = findViewById<Button>(R.id.btn_search)
         btnSearch!!.setOnClickListener {
-            repos.enqueue(object : Callback<List<Repo>>  {
-                    override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                        val adapter = CustomerAdapter(response.body()!!, {CustomerAdapter.OnItemClickListener})
-                        recyclerView.adapter = adapter
-                    }
+            repos.enqueue(object : Callback<List<Repo>> {
+                override fun onResponse(call: Call<List<Repo>>?, response: Response<List<Repo>>?) {
+//                    val adapter = CustomerAdapter(response.body()!!, this@MainActivity)
+                    listRepo = response?.body()!!
+                    recyclerView.adapter = CustomerAdapter(response?.body()!!,this@MainActivity)
+                }
 
-                    override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                override fun onFailure(call: Call<List<Repo>>?, t: Throwable) {
 
-                    }
+                }
             })
         }
     }
 
-    override fun onItemClicked(repo: Repo) {
+    override fun onItemClick(view: View, position: Int) {
         presenter.nextDiagramActivity()
     }
 }
 
-//        }
-//                val repos = service.listRepos(editTextTextPersonName.text.toString())
-//        CustomerAdapter(repos, this)
-//
-//        recyclerView.adapter = CustomerAdapter(repos, this)
-//        repos.enqueue(object : Callback<List<Repo>> {
-//            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-//                val adapter = CustomerAdapter(repos = repos, this@MainActivity)
-//                recyclerView.adapter = adapter
-//            }
-//
-//            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-//
-//            }
-//        })
-//    }
-//    interface OnItemClickListener {
-//        fun onItemClick(repo: Repo) : RecyclerView.OnItemTouchListener
-//    }
 
 
 ////https://guides.codepath.com/android/using-the-recyclerview
 ////https://developer.alexanderklimov.ru/android/views/recyclerview-kot.php
-
-//class MainActivity : BaseActivity(R.layout.activity_main), MainView, OnItemClickListener,
-//    OnPageRequestListener {
-//
-//    override val presenter: MainPresenter by providePresenter()
-//
-//    companion object {
-//        fun createLauncher() = createActivityLauncher()
-//    }
-//
-//    private lateinit var adapter: CustomerAdapter
-//    private val editTextTextPersonName: EditText by bind(R.id.editTextPersonName)
-//    val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//    override var repoList: List<Repo> = mutableListOf()
-//        set(value) {
-//
-//            field = value
-//            adapter.repos = value
-//
-//        }
-//
-//    override fun onItemClicked(repo: Repo) {
-//        presenter.nextDiagramActivity()
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        setClickListener(R.id.btn_search) {
-//            onPageRequest(1)
-//            Log.d("onClickSeatch", "search click")
-//
-//        }
-//
-//        val repos = service.listRepos(editTextTextPersonName.text.toString())
-//        CustomerAdapter(repos, this)
-//        repos.enqueue(object : Call<List<Repo>> {
-//            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-//                val adapter = CustomerAdapter(repos = repos, this@MainActivity)
-//                recyclerView?.adapter = adapter
-//            }
-//        })
-//
-////                adapter = CustomerAdapter (repoList, this)
-////        recyclerView?.adapter = adapter
-//
-//    }
-//
-//    override fun onPageRequest(page: Int) {
-//        presenter.request(editTextTextPersonName.toString())
-//    }
-//
-//    override fun getPagePreventionForEnd(): Int {
-//        return 5
-//    }
-//
-//}
